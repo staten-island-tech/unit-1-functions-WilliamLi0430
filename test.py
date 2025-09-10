d9b9fa80777f66c3e8481273a6840882e6964a2c
WilliamLi0430 <WilliamL355@nycstudents.net>
2025-09-10 09:19:52 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,24 @@
-print("hello")
+import turtle
+from turtle import *
+t = Turtle()
+t.shape('turtle')
+t.speed('fastest')
+
+def square():
+    for i in range(4):
+        t.forward(200)
+        t.left(90)
+
+square()
+
+for i in range(72):
+    square()
+    t.left(5)
+
+t.left(2.5)
+
+for i in range(72):
+    square()
+    t.left(5)
+
+turtle.done()
