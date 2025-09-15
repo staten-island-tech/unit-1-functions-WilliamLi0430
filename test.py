40e0af25fa387e66e6d29d4d53be35b80ca6a1bd
WilliamLi0430 <WilliamL355@nycstudents.net>
2025-09-15 09:26:35 -0400

    e
    e

--- test.py
+++ test.py
@@ -4,6 +4,16 @@ t = Turtle()
 t.shape('turtle')
 t.speed('fastest')
 
+def square():
+    t.forward(200)
+    t.left(90)
+    t.forward(200)
+    t.left(90)
+    t.forward(200)
+    t.left(90)
+    t.forward(200)
+    t.left(90)
+
 def star(x):
     for i in range(5):
         t.forward(x)
@@ -16,5 +26,12 @@ def doubleSquares(length):
         length += 5
         t.right(5)
 doubleSquares(5)
+
+def circlesquare():
+    for i in range(72):
+        square()
+        t.left(5)
+circlesquare()
+
 
 turtle.done()
